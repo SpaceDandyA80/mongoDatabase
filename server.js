e30c13d665614da9d39b1be1db797300aadc1379
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-08-01 15:27:52 -0700

    moving

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://workout:RussianAK47@ds259119.mlab.com:59119/heroku_9lrwwpmn", {
+  useMongoClient: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
