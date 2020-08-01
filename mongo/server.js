04b33a5384fed52b9ebe92b1526680fae619a2ba
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-08-01 14:40:34 -0700

    heroku dep

--- mongo/server.js
+++ mongo/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
 
 const PORT = 3000;
 
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://workout:RussianAK47@ds259119.mlab.com:59119/heroku_9lrwwpmn", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
